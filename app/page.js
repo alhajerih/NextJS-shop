962736a32b825753811b1a634ec263bcaa03fded
alhajerih <alhajerih@hotmail.com>
2024-10-15 21:15:17 +0300

    Task 3-6 + Bonus

--- app/page.js
+++ app/page.js
@@ -1,21 +1,12 @@
 import ProductList from "./components/ProductList";
 
 import ProductItems from "./components/ProductItems";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
-      <div className="text-color">
-        <h1>Bakery Store</h1>
-        <p>We offer the best baked goods!</p>
-      </div>
-      <div className="center-image-container">
-        <img
-          src="https://media.istockphoto.com/id/1176946222/vector/bakery-shop-building-facade-with-signboard.jpg?s=1024x1024&w=is&k=20&c=IzsGDdFgHeX-IjtCHH0zJ0wQD06Yl4CVTp01Ix2O7sw="
-          alt=""
-        />
-      </div>
-
+      <Header />
       <ProductList products={ProductItems} />
     </>
   );
