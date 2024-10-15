925820fa4a383bc8e95c0573e6498a47525e42aa
alhajerih <alhajerih@hotmail.com>
2024-10-15 16:58:51 +0300

    Task 2

--- app/page.js
+++ app/page.js
@@ -1,19 +1,34 @@
 import Image from "next/image";
+import { products } from "./products";
 
 export default function Home() {
   return (
     <>
-      <div className="text-center">
-        <div className="text-5xl">
-          <h1>Bakery store</h1>
-        </div>
-        <p>We are the best</p>
+      <div className="text-color">
+        <h1>Bakery Store</h1>
+        <p>We offer the best baked goods!</p>
       </div>
-      <div style={{ textAlign: "center" }}>
+      <div className="center-image-container">
         <img
           src="https://media.istockphoto.com/id/1176946222/vector/bakery-shop-building-facade-with-signboard.jpg?s=1024x1024&w=is&k=20&c=IzsGDdFgHeX-IjtCHH0zJ0wQD06Yl4CVTp01Ix2O7sw="
           alt=""
         />
+      </div>
+      <div className="product-list">
+        {products.map((product, index) => (
+          <div key={index} className="product-card">
+            <div className="image-container">
+              <img
+                src={product.image}
+                alt={product.name}
+                width={200}
+                height={300}
+              />
+            </div>
+            <h2 className="product-name">{product.name}</h2>
+            <p className="product-price">${product.price}KD</p>
+          </div>
+        ))}
       </div>
     </>
   );
---
+++ app/products.js
@@ -0,0 +1,14 @@
+export const products = [
+  {
+    name: "Apple pie",
+    price: 25,
+    image:
+      "https://www.southernliving.com/thmb/bbDY1d_ySIrCFcq8WNBkR-3x6pU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/2589601_Mailb_Mailbox_Apple_Pie_003-da802ff7a8984b2fa9aa0535997ab246.jpg",
+  },
+  {
+    name: "Cheescake",
+    price: 30,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxDIBKWRQWja9b128TVshvbVDNiaN__ZvyDQ&s",
+  },
+];
